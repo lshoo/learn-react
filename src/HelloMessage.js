2e0b9412e54821ca2090a250d791bd17a59af41d
廖师虎 <lshoo36@gmail.com>
2020-03-15 17:54:51 +0800

    Add chapter 2

--- src/HelloMessage.js
+++ src/HelloMessage.js
@@ -1,10 +1,56 @@
 import React from 'react';
 
+const list = [
+    { 
+        title: 'React',
+        url: 'https://facebook.github.io/react',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    }
+]
+
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) => 
+        <li>{number}</li>
+    );
+
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
 class HelloMessage extends React.Component {
+    
     render() {
+        let msg = "Welcome React World!";
+        msg = "Hello, " + msg;
         return (
             <div> 
+                <h2>{msg}</h2>
                 Hello {this.props.name}
+                { list.map(function(item) {
+                    return (
+                        <div key={item.objectID}>
+                            <span>
+                                <a href={item.url}>{item.title}</a>
+                            </span>
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                        </div>
+                    );
+                })}
+                
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import HelloMessage from './HelloMessage';
+import NumberList from './NumberList';
 import Timer from './Timer';
 import TodoApp from './TodoApp';
 
@@ -12,6 +13,12 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<HelloMessage name="English"/>, document.getElementById('app'));
 
+const numbers = [1, 2, 3, 4, 5, 100];
+ReactDOM.render(
+    <NumberList numbers={numbers} />,
+    document.getElementById('nl')
+);
+
 ReactDOM.render(<Timer />, document.getElementById('timer'));
 
 ReactDOM.render(<TodoApp />, document.getElementById('todo'));
@@ -20,3 +27,7 @@ ReactDOM.render(<TodoApp />, document.getElementById('todo'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+if (module.hot) {
+    module.hot.accept();
+}
