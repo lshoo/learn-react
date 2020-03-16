247f7c40a9d48d69286416d3ac6609b74b9d9a64
liaoshifu <lshoo36@gmail.com>
2020-03-16 10:13:58 +0800

    js var

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var msg = "Welcome React World!!!"
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>{msg}</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/HelloMessage.js
+++ src/HelloMessage.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 class HelloMessage extends React.Component {
     render() {
+        var msg = "Welcome React World!!!";
+
         return (
             <div> 
+                <h2>{msg}</h2>
                 Hello {this.props.name}
             </div>
         );
